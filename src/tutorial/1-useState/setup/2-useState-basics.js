7442ab2c81f7b4b2071d15a4cb94f2df27333c55
Michal Boczula <michal.boczula0101@gmail.com>
2021-11-03 19:50:43 +0100

    useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [title, setTitle] = useState('Default title');
+
+  const changeTitle = function()
+  {
+    if(title === 'Default title')
+    {
+      setTitle('NOT Default title');
+    }
+    else
+    {
+      setTitle('Default title');
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button type='button' className='btn'  onClick={changeTitle}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,40 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const cleanUp = function()
+  { 
+    setPeople([]);
+  }
+
+  const removeItem = function(id)
+  {
+    setPeople(
+      people.filter(ele => ele.id !== id));
+  };
+
+  // use data not dom 
+  // const play = function(e)
+  // {
+  //   console.log(e.target.parentNode);
+  //   e.target.parentNode.remove();
+  // }
+
+  return <React.Fragment>
+    {
+      people.map(ele => 
+      {
+        const {id, name} = ele;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick ={() => removeItem(id)}>remove</button>
+          </div>)
+      })
+    }
+    <button className='btn' onClick={cleanUp}>Remove All</button>
+  </React.Fragment>
 };
 
 export default UseStateArray;
